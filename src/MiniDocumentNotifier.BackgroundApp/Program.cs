2afdf82cdb0b240dfec3011c106180245dcfe37b
Mihail Musteata <mihail.musteata@isa.utm.md>
2026-08-04 11:10:24 +0300

    initial project setup

---
+++ src/MiniDocumentNotifier.BackgroundApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniDocumentNotifier.BackgroundApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
