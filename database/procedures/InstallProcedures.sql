333771f075ad92b99d2768951ff7826dec91c6b5
Mihail Musteata <mihail.musteata@isa.utm.md>
2026-08-18 16:05:11 +0300

    init setup for testing

--- database/procedures/InstallProcedures.sql
+++ database/procedures/InstallProcedures.sql
@@ -5,3 +5,7 @@ GO
 :r "C:\Projects\mini-document-notifier\database\procedures\sp_InstitutionGetById.sql"
 :r "C:\Projects\mini-document-notifier\database\procedures\sp_UserGetByUsernameAndInstitution.sql"
 :r "C:\Projects\mini-document-notifier\database\procedures\sp_UserRegister.sql"
+:r "C:\Projects\mini-document-notifier\database\procedures\sp_DocumentGetByInstitution.sql"
+:r "C:\Projects\mini-document-notifier\database\procedures\sp_DocumentGetPaged.sql"
+:r "C:\Projects\mini-document-notifier\database\procedures\sp_DocumentInsert.sql"
+:r "C:\Projects\mini-document-notifier\database\procedures\sp_ViewConfigurationGetAllWithInstitutions.sql"
---
+++ tests/MiniDocumentNotifier.Application.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MiniDocumentNotifier.Application.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MiniDocumentNotifier.Application.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("540357db-5cbf-4faf-8f90-208a08d8220b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
